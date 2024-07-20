2730902d833eb8ef1c5ab2b8f3065aa56eb66a2f
suhail25 <suhailkhan250899@gmail.com>
2024-07-20 13:52:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,187 @@
+// set up canvas
+const score = document.createElement("p");
+document.body.appendChild(score);
+let livescore = 0;// Below heading - show score value in <p> element
+
+const canvas = document.querySelector("canvas"); // getting the reference of the canvas element
+const ctx = canvas.getContext("2d");
+// (ctx) is the object that directly represents the drawing area of the canvas and allows us to draw 2D shapes on it.
+
+const width = (canvas.width = window.innerWidth); //width and height of browser web page
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random RGB color value
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Shape {
+    constructor(x, y, velX, velY) {
+        this.x = x;
+        this.y = y;
+        this.velX = velX;
+        this.velY = velY;
+    }
+}
+
+class EvilCircle extends Shape{
+    constructor(x, y, velX, velY, color, size) {
+        super(x, y, 20, 20);
+        this.color = 'rgb(255, 255, 255)';
+        this.size = random(10, 20);
+
+        window.addEventListener("keydown", (e) => {
+          switch (e.key) {
+            case "a":
+              this.x -= this.velX;
+              break;
+            case "d":
+              this.x += this.velX;
+              break;
+            case "w":
+              this.y -= this.velY;
+              break;
+            case "s":
+              this.y += this.velY;
+              break;
+          }
+        });
+        
+    }
+    draw() {  //a series of members of the 2D canvas context
+      ctx.beginPath();
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+
+    update() {
+      if (this.x + this.size >= width) {
+        this.x -= this.size;
+      }
+  
+      if (this.x - this.size <= 0) {
+        this.x += this.size;
+      }
+  
+      if (this.y + this.size >= height) {
+        this.y -= this.size;
+      }
+  
+      if (this.y - this.size <= 0) {
+        this.y += this.size;
+      }
+    }
+
+    collisionDetect() {
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+            score.textContent = `Balls Left: ${--livescore}`;
+          }
+        }
+      }
+    }
+}
+
+class Ball extends Shape{
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -Math.abs(this.velX);
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = Math.abs(this.velX);
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  
+}
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+  score.textContent = `Ballls Left: ${++livescore}`;
+}
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    if (ball.exists) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();}
+  }
+  evilBall.draw();
+  evilBall.update();
+  evilBall.collisionDetect();
+ requestAnimationFrame(loop);
+}
+const evilBall = new EvilCircle(random(1, width), random(1, width));
+loop();
